227607d30d35c724a3f396cb7dca01ce2783d43a
ya0yy <ya0yy@users.noreply.github.com>
2018-08-30 15:52:47 +0800

    添加了一个

---
+++ microservice-file-upload/src/main/java/com/yao/microservicefileupload/upload/TestGit.java
@@ -0,0 +1,33 @@
+package com.yao.microservicefileupload.upload;
+
+/**
+ * 　　　　　　　 ┏┓　 ┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　┃
+ * 　　　　　　　┃　　　━　　 ┃ ++ + + +
+ * 　　　　　　 ████━████  ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　 ┃　　　┃
+ * 　　　　　　　　 ┃　　　┃ + + + +
+ * 　　　　　　　　 ┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　　 ┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　　　　 ┃　　　┃
+ * 　　　　　　　　 ┃　　　┃　　+
+ * 　　　　　　　　 ┃　 　 ┗━━━┓ + +
+ * 　　　　　　　　 ┃ 　　　　   ┣┓
+ * 　　　　　　　　 ┃ 　　　　　 ┏┛
+ * 　　　　　　　　 ┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　  ┃┫┫ ┃┫┫
+ * 　　　　　　　　  ┗┻┛ ┗┻┛+ + + +
+ *
+ * @program: microservice-parent
+ * @description: s
+ * @author: YaoYY
+ * @create: 2018-08-30 15:52
+ */
+public class TestGit {
+
+}
